f8e0dcf9da40064417ca0e72f47d816cd95edd95
Dmitry Snitko <user_M6@softline.com>
2018-12-20 12:40:39 +0300

    Changed_script_for_creating_VMs

--- scripts/create_infrustructure.sh
+++ scripts/create_infrustructure.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-image_id=ami-09693313102a30b2c
-instance_type=t2.micro
-vpc_id=vpc-0e4dfd64aef6a44fc
-key_name=user6
-shutdown_type=stop
-security_group=...
-subnet_id=subnet-0a8edbfe14d168679
-tags="ResourceType=instance,Tags=[{Key=Name,Value=user6-1}]"
+IMAGE_ID=ami-02fc24d56bc5f3d67 #ami-09693313102a30b2c
+INSTANCE_TYPE=t2.micro
+VPC_ID=vpc-0e4dfd64aef6a44fc
+KEY_NAME=user6
+USER_NAME=user6-vpc3
+SHUTDOWN_TYPE=stop
+SECURITY_GROUP=sg-0f0cc75216aa3485b
+SUBNET_ID=subnet-0a8edbfe14d168679
+TAGS="ResourceType=instance,Tags=[{Key=Name,Value=NAME},{Key=tag,Value=${KEY_NAME}}]"
 
-start()
+start_vm()
 {
 
-  private_ip_addr="10.3.1.61"
-  public_ip=associate-public-ip-address
+  local private_ip_addr="$1"
+  local public_ip="$2"
+  local name="$3"
+
+  local tags=$(echo $TAGS | sed s/NAME/$name/)
+  #local tags=$(TAGS/NAME/$name)
 
   aws ec2 run-instances \
-    --image-id "$image_id" \
-    --instance-type "$instance_type" \
-    --key-name "$key_name" \
-    --subnet-id "$subnet_id" \
-    --instance-initiated-shutdown-behavior "$shutdown_type" \
+    --image-id "$IMAGE_ID" \
+    --instance-type "$INSTANCE_TYPE" \
+    --key-name "$KEY_NAME" \
+    --subnet-id "$SUBNET_ID" \
+    --instance-initiated-shutdown-behavior "$SHUTDOWN_TYPE" \
     --private-ip-address "$private_ip_addr" \
     --tag-specifications "$tags" \
-    --${public_ip}
+    --${public_ip} \
+    --security-group-id "$SECURITY_GROUP"
 
     #--elastic-gpu-specification <value> \
     #--elastic-inference-accelerators <value> \
@@ -32,11 +38,46 @@ start()
     #--additional-info <value> \
 }
 
+get_dns_name()
+{
+  local instances="$1"
+
+  aws ec2 describe-instances --instance-ids ${instance} \
+  | jq -r '.Reservations[0].Instances[0].NetworkInterfaces[0].Association.PublicDnsName'
+}
+
+start ()
+{
+#  local vm_number="$1"
+#  
+#  if [ "$1" < "0" ]; then
+#    echo "Number of VMs should be more then 1"
+#  elif [ "$1" = "1" ]; then 
+
+#  start_log=$(
+    start_vm 10.3.1.61 associate-public-ip ${USER_NAME}-vm1 >> ~/start_log
+#  )
+  sleep 5s
+
+  instance_id=$( cat ~/start_log | jq -r .Instances[0].InstanceId)
+
+#  else
+#    for i in {2..2}; do
+#      start_vm 10.3.1.$((60+i)) no-associate-public-ip-address ${USER_NAME}-vm$i > /dev/null
+#    done
+
+  sleep 10s
+  dns_name=$(get_dns_name "$instance_id")
+  echo $dns_name
+
+#fi
+}
+
 stop()
 {
   ids=($(
     aws ec2 describe-instances \
-    --query 'Reservations[*].Instances[?KeyName==`'$key_name'`].InstanceId' \
+    --query 'Reservations[*].Instances[?KeyName==`'$KEY_NAME'`].InstanceId' \
     --output text
   ))
   aws ec2 terminate-instances --instance-ids "${ids[@]}"
