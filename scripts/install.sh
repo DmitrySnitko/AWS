add5b1377da4e9d773500732ac1e98492e62d8b1
Dmitry Snitko <user_M6@softline.com>
2018-12-20 16:27:20 +0300

    Install script error correction

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,7 +15,7 @@ ve/bin/pip install -r requirements.txt
 #sudo cp config/qrencode.conf /etc/nginx/sites-available/default
 sudo curl https://raw.githubusercontent.com/DmitrySnitko/AWS/master/config/qrencode.conf > /etc/nginx/sites-available/default/grencode.conf
 
-mkdir -p ~/log/
-nohup ve/bin/python bin/srv.py >> ~/log/qrencode.log 2>&1 &
+mkdir -p "$DIR"/log/
+nohup ve/bin/python bin/srv.py >> "$DIR"/log/qrencode.log 2>&1 &
 
 sudo /etc/init.d/nginx restart
